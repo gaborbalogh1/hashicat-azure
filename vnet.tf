07cfc742ee6d0a536846d42c6b130bad5baec5ac
Gabor Balogh <gabor.balogh1@gmail.com>
2021-05-21 14:50:02 +0100

    Added vnet module
    Added vnet module

--- vnet.tf
+++ vnet.tf
@@ -2,5 +2,5 @@ module "vnet" {
   source  = "app.terraform.io/GaborBalogh-training/vnet/azurerm"
   version = "2.4.0"
   # insert required variables here
-  resource_group_name = "test"
+  resource_group_name = "GaborBalogh"
 }
